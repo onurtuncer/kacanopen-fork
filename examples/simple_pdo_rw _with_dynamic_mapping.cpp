610c75c02cc905cd93021a93f5f5cd895bc4cfb6
xdaco <xdaco.xinu@gmail.com>
2018-07-09 19:51:17 +0200

    Edited the readme file for the example codes.

---
+++ examples/simple_pdo_rw _with_dynamic_mapping.cpp
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2015-2016, Thomas Keh
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    3. Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <signal.h>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include "../utils/include/parse_sdo.h"
+#include "../utils/src/parse_sdo.cpp"
+#include "canopen_error.h"
+#include "core.h"
+#include "device.h"
+#include "logger.h"
+#include "master.h"
+#include "receive_pdo_mapping.h"
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) { keepRunning = 0; }
+
+void qry_abspeed_channel_1_callback(const kaco::ReceivePDOMapping& mapping,
+                                    std::vector<uint8_t> data) {
+  std::cout << "hola" << std::endl;
+}
+
+int main() {
+  // Signal handleing
+  signal(SIGINT, intHandler);
+
+  // ----------- //
+  // Preferences //
+  // ----------- //
+
+  // A Roboteq motor driver was used to test this program //
+
+  // The node ID of the slave we want to communicate with.
+  const uint8_t node_id = 4;
+
+  // Set the name of your CAN bus. "slcan0" is a common bus name
+  // for the first SocketCAN device on a Linux system.
+  const std::string busname = "can0";
+
+  // Set the baudrate of your CAN bus. Most drivers support the values
+  // "1M", "500K", "125K", "100K", "50K", "20K", "10K" and "5K".
+  const std::string baudrate = "500K";
+
+  // -------------- //
+  // Initialization //
+  // -------------- //
+  // Create core.
+  kaco::Core core;
+  std::cout << "Starting Core (connect to the driver and start the receiver "
+               "thread)..."
+            << std::endl;
+  if (!core.start(busname, baudrate)) {
+    std::cout << "Starting core failed." << std::endl;
+    return EXIT_FAILURE;
+  }
+  // Create device pointer
+  std::shared_ptr<kaco::Device> device;
+  // This will be set to true by the callback below.
+  bool found_node = false;
+  bool node_initialized = false;
+  std::cout << "Registering a callback which is called when a device is "
+               "detected via NMT..."
+            << std::endl;
+  core.nmt.register_device_alive_callback(
+      [&](const uint8_t new_node_id) mutable {
+        // Check if this is the node we are looking for.
+        if (new_node_id == node_id) {
+          if (!found_node) {
+            found_node = true;
+
+            device.reset(new kaco::Device(core, node_id));
+            device->load_dictionary_from_eds(
+                "/home/mhs/Desktop/EDS/roboteq_motor_controllers_v80beta.eds");
+            device->add_transmit_pdo_mapping(0x200 + node_id, {{"Cmd_DOUT", 0}},
+                                             kaco::TransmissionType::ON_CHANGE,
+                                             std::chrono::milliseconds(250));
+            //            device->add_receive_pdo_mapping(
+            //                0x180 + node_id, "qry_abspeed/channel_1", 1,
+            //                qry_abspeed_channel_1_callback);  // offset 1,
+            device->add_receive_pdo_mapping(
+                0x180 + node_id, "qry_abspeed/channel_1", 1);  // offset 1,
+            device->add_receive_pdo_mapping(
+                0x180 + node_id, "qry_abspeed/channel_2", 2);  // offset 2,
+            device->add_receive_pdo_mapping(
+                0x180 + node_id, "qry_batamps/channel_1", 4);  // offset 4,
+            device->add_receive_pdo_mapping(
+                0x180 + node_id, "qry_batamps/channel_2", 6);  // offset 6,
+            node_initialized = true;
+          }
+        }
+      });
+  uint8_t digtal_out_write = 0x0;
+  while (keepRunning) {
+    if (node_initialized) {
+
+      try {
+        DUMP_HEX(
+            device->get_entry("qry_abspeed/channel_1",
+                              kaco::ReadAccessMethod::pdo_request_and_wait));
+        DUMP_HEX(
+            device->get_entry("qry_abspeed/channel_2",
+                              kaco::ReadAccessMethod::pdo_request_and_wait));
+        DUMP_HEX(
+            device->get_entry("qry_batamps/channel_1",
+                              kaco::ReadAccessMethod::pdo_request_and_wait));
+        DUMP_HEX(
+            device->get_entry("qry_batamps/channel_2",
+                              kaco::ReadAccessMethod::pdo_request_and_wait));
+      } catch (kaco::canopen_error exception) {
+      }
+      digtal_out_write++;
+      device->set_entry("Cmd_DOUT", digtal_out_write,
+                        kaco::WriteAccessMethod::pdo);
+      if (0xFF == digtal_out_write) {
+        digtal_out_write = 0x0;
+      }
+    }
+  }
+  std::cout << "Finished." << std::endl;
+  return EXIT_SUCCESS;
+}
