464260c5e50381ef2044fdc146db93e646d65809
xdaco <xdaco.xinu@gmail.com>
2020-03-14 15:22:40 +0100

    reverted the temporary changes of defult CiA eds entries

--- src/master/device.cpp
+++ src/master/device.cpp
@@ -601,22 +601,21 @@ void Device::load_dictionary_from_eds(const std::string& path) {
 
   // Load generic names from the standard CiA profiles on top of the existing
   // dictionary.
-  //  if (m_eds_library.ready()) {
-  //    // Wo know nothing about the EDS... No mandatory entries here. At least
-  //    // 0x1000 is required for load_cia_dictionary():
-  //    if (!has_entry(0x1000)) {
-  //      add_entry(0x1000, 0, "device_type", Type::uint32,
-  //      AccessType::read_only);
-  //    }
-  //    Config::eds_reader_just_add_mappings = true;
-  //    load_cia_dictionary();
-  //    Config::eds_reader_just_add_mappings = false;
-  //  } else {
-  //    WARN(
-  //        "[Device::load_dictionary_from_eds] Cannot load generic entry names
-  //        "
-  //        "because EDS library is not available.");
-  //  }
+  if (m_eds_library.ready()) {
+    // Wo know nothing about the EDS... No mandatory entries here. At least
+    // 0x1000 is required for load_cia_dictionary():
+    if (!has_entry(0x1000)) {
+      add_entry(0x1000, 0, "device_type", Type::uint32,
+      AccessType::read_only);
+    }
+    Config::eds_reader_just_add_mappings = true;
+    load_cia_dictionary();
+    Config::eds_reader_just_add_mappings = false;
+  } else {
+    WARN(
+        "[Device::load_dictionary_from_eds] Cannot load generic entry names"
+        "because EDS library is not available.");
+  }
 }
 
 bool Device::load_operations() {
